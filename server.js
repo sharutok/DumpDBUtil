e715e2f80e1f0807680ebdeb0071d7f3ff567765
Sharan Kudtarkar <SK19@ador.co.in>
2023-05-04 23:12:28 +0530

    05/04/23 22:45:50

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const app = require("./App");
-const PORT = process.env.NODE_ENV === "production" ? 8010 : 5555;
+require("dotenv").config();
 
-app.listen(PORT, () => {
-    console.log(`app listining to port ${PORT}`);
-})
+if (process.env.NODE_ENV === "production") {
+    port = 3075;
+} else {
+    port = 8111;
+}
+
+app.listen(port, () => {
+    console.log(`listining to port ${port}...`);
+});
